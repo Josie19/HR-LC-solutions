81374f555727b5d070e850440a38555519ebae15
Jose Luiz Magallanes <006319894@coyote.csusb.edu>
2021-02-27 00:23:38 -0800

    new 2

--- alternatingCharacters.cpp
+++ alternatingCharacters.cpp
@@ -1,1 +1,20 @@
+/*constraints: 
+ * s contains only As and Bs
+ * s length < 10^5
+ * queries < 10
+ * only iterate non-empty strings greater than 1
+*/
 #include<iostream>
+using namespace std;
+/*-------alternatingCharacters-----*/
+int alternatingCharacters(string s){
+int i = 0, j = i+1, del = 0;
+//no deletions if only single character in s
+if(s.length() == 1) return del;
+//iterate characters using double pointer method
+while(j < s.length()){
+	if(s[i] == s[j]) del++;
+	i++,j++;
+}
+return del;
+}
